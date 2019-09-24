6db67c7bd48560cff82b82d42e7297e70ccfc7ba
Romain KIRSZBAUM <rkirszba@e2r6p15.42.fr>
2019-09-24 12:15:13 +0200

    clean

--- obj/draw_object_utils.d
+++
@@ -1,2 +0,0 @@
-obj/draw_object_utils.o: src/draw_object_utils.c includes/fdf.h \
-  libft/includes/libft.h libmlx/includes/libmlx.h
--- obj/reinit_matrices.d
+++
@@ -1,2 +0,0 @@
-obj/reinit_matrices.o: src/reinit_matrices.c includes/fdf.h \
-  libft/includes/libft.h libmlx/includes/libmlx.h
