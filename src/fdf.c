97c26eeb8cf7c6478abf9ca6da401ded280160ff
Romain KIRSZBAUM <rkirszba@e2r7p17.42.fr>
2019-09-27 16:49:44 +0200

    final version

--- src/fdf.c
+++ src/fdf.c
@@ -6,7 +6,7 @@
 /*   By: allefebv <allefebv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/14 18:22:04 by rkirszba          #+#    #+#             */
-/*   Updated: 2019/09/27 16:11:04 by rkirszba         ###   ########.fr       */
+/*   Updated: 2019/09/27 16:47:43 by rkirszba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ int			main(int ac, char **av)
 		return (print_sys_error(errno));
 	ft_bzero(&fdf, sizeof(t_fdf));
 	ret = parse_map(&fdf, fd);
+	close(fd);
 	if (!ret)
 		ret = init_fdf(&fdf);
 	if (!ret)
