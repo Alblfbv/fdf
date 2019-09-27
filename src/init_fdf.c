208de750ce1212ae79c5788685ecb224f0ad8f14
Romain KIRSZBAUM <rkirszba@e2r7p17.42.fr>
2019-09-27 15:07:07 +0200

    base scale

--- src/init_fdf.c
+++ src/init_fdf.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init_fdf.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: allefebv <allefebv@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rkirszba <rkirszba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/16 11:37:17 by rkirszba          #+#    #+#             */
-/*   Updated: 2019/09/26 20:06:08 by allefebv         ###   ########.fr       */
+/*   Updated: 2019/09/27 14:58:49 by rkirszba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,23 +46,13 @@ static void	compute_shifts(t_fdf *fdf)
 	double	limit1;
 	double	limit2;
 
-	if (fdf->nb_cols == 1)
-		fdf->shift_x = IMG_WDTH / 2;
-	else
-	{
-		limit1 = -cos((double)(M_PI / 6)) * fdf->nb_rows * fdf->base_scale;
-		limit2 = cos((double)(M_PI / 6)) * fdf->nb_cols * fdf->base_scale;
-		fdf->shift_x = (int)(-limit1) + (IMG_WDTH - (int)(limit2 - limit1)) / 2;
-	}
-	if (fdf->nb_rows == 1)
-		fdf->shift_y = IMG_HGHT / 2;
-	else
-	{
-		limit1 = 0;
-		limit2 = sin((double)(M_PI / 6))\
-			* (fdf->nb_cols + fdf->nb_rows) * fdf->base_scale;
-		fdf->shift_y = (IMG_HGHT - (int)(limit2 - limit1)) / 2;
-	}
+	limit1 = -cos((double)(M_PI / 6)) * (fdf->nb_rows - 1) * fdf->base_scale;
+	limit2 = cos((double)(M_PI / 6)) * (fdf->nb_cols - 1) * fdf->base_scale;
+	fdf->shift_x = (int)(-limit1) + (IMG_WDTH - (int)(limit2 - limit1)) / 2;
+	limit1 = 0;
+	limit2 = sin((double)(M_PI / 6))\
+		* (fdf->nb_cols + fdf->nb_rows - 2) * fdf->base_scale;
+	fdf->shift_y = (IMG_HGHT - (int)(limit2 - limit1)) / 2;
 }
 
 static void	compute_base_scale(t_fdf *fdf)
@@ -72,24 +62,14 @@ static void	compute_base_scale(t_fdf *fdf)
 	double	limit1;
 	double	limit2;
 
-	if (fdf->nb_cols == 1)
-		scale_x = 0xFFFFFFFF;
-	else
-	{
-		limit1 = -cos((double)(M_PI / 6)) * fdf->nb_rows;
-		limit2 = cos((double)(M_PI / 6)) * fdf->nb_cols;
-		scale_x = (IMG_WDTH - 2 * X_SHIFT) / ((int)(limit2 - limit1) == 0 ?\
-			1 : (int)(limit2 - limit1));
-	}
-	if (fdf->nb_rows == 1)
-		scale_y = 0xFFFFFFFF;
-	else
-	{
-		limit1 = 0;
-		limit2 = sin((double)(M_PI / 6)) * (fdf->nb_cols + fdf->nb_rows);
-		scale_y = (IMG_HGHT - 2 * Y_SHIFT) / ((int)(limit2 - limit1) == 0 ?\
-			1 : (int)(limit2 - limit1));
-	}
+	limit1 = -cos((double)(M_PI / 6)) * (fdf->nb_rows - 1);
+	limit2 = cos((double)(M_PI / 6)) * (fdf->nb_cols - 1);
+	scale_x = (IMG_WDTH - 2 * X_SHIFT) / ((int)(limit2 - limit1) == 0 ?\
+		1 : (int)(limit2 - limit1));
+	limit1 = 0;
+	limit2 = sin((double)(M_PI / 6)) * (fdf->nb_cols + fdf->nb_rows - 2);
+	scale_y = (IMG_HGHT - 2 * Y_SHIFT) / ((int)(limit2 - limit1) == 0 ?\
+		1 : (int)(limit2 - limit1));
 	fdf->base_scale = (scale_x > scale_y ? scale_y : scale_x);
 }
 
